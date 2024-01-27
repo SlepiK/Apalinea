47ce545d955636628dadd25519e40ec67202a5d8
SlepiK <Simon.Stahmer1@uni-oldenburg.de>
2024-01-27 17:50:29 +0100

    IOperator

---
+++ include/Operator/IOperator.hpp
@@ -0,0 +1,16 @@
+//
+// Created by SlepiK on 27.01.2024.
+//
+
+#ifndef STREAMT_V1_OPERATOR_IOPERATOR_HPP
+#define STREAMT_V1_OPERATOR_IOPERATOR_HPP
+
+namespace Stream::V1::Operator {
+
+    class IOperator {
+
+    };
+
+} // Stream::V1::Operator
+
+#endif //STREAMT_IOPERATOR_HPP
