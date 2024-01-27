11b40194833980932f32384ca5b6ecbf2b0d684c
SlepiK <Simon.Stahmer1@uni-oldenburg.de>
2024-01-27 18:05:41 +0100

    Changing of defintion

---
+++ include/Operator/AbstractOperator.hpp
@@ -0,0 +1,16 @@
+//
+// Created by SlepiK on 27.01.2024.
+//
+
+#ifndef STREAM_V1_OPERATOR_ABSTRACTOPERATOR_HPP
+#define STREAM_V1_OPERATOR_ABSTRACTOPERATOR_HPP
+
+namespace Stream::V1::Operator {
+
+    class AbstractOperator {
+
+    };
+
+} // Stream::V1::Operator
+
+#endif //STREAM_V1_OPERATOR_ABSTRACTOPERATOR_HPP
