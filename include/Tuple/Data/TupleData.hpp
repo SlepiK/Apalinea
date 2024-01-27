50eb6092833beacd3b711998f737d6afa486f497
SlepiK <Simon.Stahmer1@uni-oldenburg.de>
2024-01-27 18:17:25 +0100

    Changing of defintion

--- include/Tuple/Data/TupleData.hpp
+++ include/Tuple/Data/TupleData.hpp
@@ -5,10 +5,17 @@
 #ifndef STREAM_V1_TUPLE_TUPLEDATA_HPP
 #define STREAM_V1_TUPLE_TUPLEDATA_HPP
 
+#include <string>
+
 namespace Stream::V1::Tuple {
 
+    template<typename Tdata>
     class TupleData {
-
+    public:
+    private:
+        Tdata vData;
+        std::string vName;
+    protected:
     };
 
 } // Stream::V1::Tuple
--- include/Tuple/Tuple.hpp
+++ include/Tuple/Tuple.hpp
@@ -1,5 +1,5 @@
 //
-// Created by simon on 27.01.2024.
+// Created by SlepiK on 27.01.2024.
 //
 
 #ifndef STREAM_V1_TUPLE_TUPLE_HPP
