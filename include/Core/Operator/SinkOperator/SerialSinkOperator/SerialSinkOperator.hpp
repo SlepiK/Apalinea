2e5bf01046fd809b4d3a091f2574705a5e3e00d9
Simon <simon.stahmer1@uni-oldenburg.de>
2024-01-29 14:38:34 +0100

    SerialSinkOperator.hpp implementation for ESP

--- include/Core/Operator/SinkOperator/SerialSinkOperator/SerialSinkOperator.hpp
+++ include/Core/Operator/SinkOperator/SerialSinkOperator/SerialSinkOperator.hpp
@@ -5,12 +5,26 @@
 #ifndef STREAM_V1_CORE_OPERATOR_SINKOPERATOR_SERIALSINKOPERATOR_HPP
 #define STREAM_V1_CORE_OPERATOR_SINKOPERATOR_SERIALSINKOPERATOR_HPP
 
+#include "Operator/SinkOperator/AbstractSinkOperator.hpp"
+#include "Tuple/Tuple.hpp"
+
+#ifdef ENERGYLEAF_ESP
+#include <HardwareSerial.h>
+#endif
+
 namespace Energyleaf::Stream::V1::Core::Operator::SinkOperator {
 
-    class SerialSinkOperator {
+    class SerialSinkOperator
+            : public Energyleaf::Stream::V1::Operator::AbstractSinkOperator<Energyleaf::Stream::V1::Tuple::Tuple<std::string>> {
     public:
     private:
     protected:
+        void work(Energyleaf::Stream::V1::Tuple::Tuple<std::string> &inputTuple) override {
+#ifdef ENERGYLEAF_ESP
+            Serial.print("New Tuple: ");
+            Serial.println(inputTuple.getItem<std::string>(0).getData().c_str());
+#endif
+        }
     };
 
 } // Stream::V1::Core::Operator::SinkOperator
