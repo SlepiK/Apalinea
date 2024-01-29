989bf11ae36e53c81fab3000437fa6f99dcb1c4c
SlepiK <Simon.Stahmer1@uni-oldenburg.de>
2024-01-29 03:12:08 +0100

    Working example

---
+++ include/Core/Constants/Version.hpp
@@ -0,0 +1,13 @@
+//
+// Created by SlepiK on 29.01.2024.
+//
+
+#ifndef STREAM_VERSION_HPP
+#define STREAM_VERSION_HPP
+
+namespace Stream::Constants::Version {
+    constexpr char VERSION[] = "V1.0";
+    constexpr char VERSIONNAME[] = "Claudi";
+}
+
+#endif //STREAM_VERSION_HPP
--- main.cpp
+++ main.cpp
@@ -3,21 +3,12 @@
 //
 
 #include "Core/Operator/SinkOperator/CoutSinkOperator/CoutSinkOperator.hpp"
-#include "include/Operator/SinkOperator/AbstractSinkOperator.hpp"
 #include "Core/Operator/SourceOperator/StringDemoSourceOperator/StringDemoSourceOperator.hpp"
-#include "include/Tuple/Tuple.hpp"
 
 #include "Core/Plan/Plan.hpp"
 
 int main(int argc, char *argv[])
 {
-    Stream::V1::Tuple::Tuple<std::string> text;
-    text.addItem(std::string("TestText"),std::string("Was geht"));
-    Stream::V1::Core::Operator::SinkOperator::CoutSinkOperator coutSinkOperator;
-    coutSinkOperator.process(text);
-    coutSinkOperator.process(text);
-    coutSinkOperator.process(text);
-    coutSinkOperator.process(text);
     Stream::V1::Core::Plan::Plan plan;
     auto sourcelink = plan.createLink(Stream::V1::Link::make_SourceLinkUPtr<Stream::V1::Core::Operator::SourceOperator::StringDemoSourceOperator>());
     auto sinklink = plan.createLink(Stream::V1::Link::make_SinkLinkUPtr<Stream::V1::Core::Operator::SinkOperator::CoutSinkOperator>());
