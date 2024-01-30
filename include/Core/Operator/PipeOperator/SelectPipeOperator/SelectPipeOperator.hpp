0c32622c7424d476cb99937d4faf9168d811b8ee
Simon <simon.stahmer1@uni-oldenburg.de>
2024-01-30 13:49:36 +0100

    SelectPipeOperator.hpp

---
+++ include/Core/Operator/PipeOperator/SelectPipeOperator/SelectPipeOperator.hpp
@@ -0,0 +1,35 @@
+//
+// Created by SlepiK on 30.01.24.
+//
+
+#ifndef STREAM_V1_CORE_OPERATOR_PIPEOPERATOR_SELECTPIPEOPERATOR_HPP
+#define STREAM_V1_CORE_OPERATOR_PIPEOPERATOR_SELECTPIPEOPERATOR_HPP
+
+#include "Operator/PipeOperator/AbstractPipeOperator.hpp"
+#include "Tuple/Tuple.hpp"
+
+namespace Energyleaf::Stream::V1::Core::Operator::PipeOperator {
+    class SelectPipeOperator
+            : public Energyleaf::Stream::V1::Operator::AbstractPipeOperator<Energyleaf::Stream::V1::Tuple::Tuple<std::size_t>,
+                    Energyleaf::Stream::V1::Tuple::Tuple<bool>> {
+    public:
+        void setThreshold(int&& threshold) {
+            this->vThreshold = threshold;
+        }
+
+        [[nodiscard]] const int& getThreshold() const {
+            return this->vThreshold;
+        }
+    private:
+        int vThreshold = 0;
+    protected:
+        void work(Energyleaf::Stream::V1::Tuple::Tuple<std::size_t> &inputTuple,
+                  Energyleaf::Stream::V1::Tuple::Tuple<bool> &outputTuple) override {
+            size_t input = inputTuple.getItem<std::size_t>(0).getData();
+            outputTuple.clear();
+            outputTuple.addItem(std::string("SELECT"), input > this->vThreshold);
+        }
+    };
+}
+
+#endif //STREAM_V1_CORE_OPERATOR_PIPEOPERATOR_SELECTPIPEOPERATOR_HPP
