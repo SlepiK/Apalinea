236e8c347c207b3e4ebeec7f6729e603272ef831
Simon <simon.stahmer1@uni-oldenburg.de>
2024-02-13 05:28:05 +0100

    fix

--- include/Core/Operator/PipeOperator/CropPipeOperator/CropPipeOperator.hpp
+++ include/Core/Operator/PipeOperator/CropPipeOperator/CropPipeOperator.hpp
@@ -2,59 +2,66 @@
 // Created by SlepiK on 29.01.24.
 //
 
-#ifndef STREAM_V1_CORE_OPERATOR_PIPEOPERATOR_CROPPIPEOPERATOR_HPP
-#define STREAM_V1_CORE_OPERATOR_PIPEOPERATOR_CROPPIPEOPERATOR_HPP
+#ifndef STREAM_V1_CORE_OPERATOR_PIPEOPERATOR_DETECTORPIPEOPERATOR_HPP
+#define STREAM_V1_CORE_OPERATOR_PIPEOPERATOR_DETECTORPIPEOPERATOR_HPP
 
-#include <algorithm>
 #include "Operator/PipeOperator/AbstractPipeOperator.hpp"
-#include "Types/Image/Image.hpp"
 #include "Tuple/Tuple.hpp"
+#include "Types/Image/Image.hpp"
+#include "Types/Pixel/HSV.hpp"
+#include "Types/Pixel/RGB.hpp"
+#include "Extras/Converter/Types/Pixel/RGBtoHSV.hpp"
+#include <utility>
 
 namespace Energyleaf::Stream::V1::Core::Operator::PipeOperator {
-    class CropPipeOperator
-            : public Energyleaf::Stream::V1::Operator::AbstractPipeOperator<Energyleaf::Stream::V1::Tuple::Tuple<Energyleaf::Stream::V1::Types::Image>,
-                    Energyleaf::Stream::V1::Tuple::Tuple<Energyleaf::Stream::V1::Types::Image>> {
+    class DetectorPipeOperator
+            : public Energyleaf::Stream::V1::Operator::AbstractPipeOperator<Energyleaf::Stream::V1::Tuple::Tuple<Energyleaf::Stream::V1::Types::Image,std::string>,
+                    Energyleaf::Stream::V1::Tuple::Tuple<std::size_t,std::string>> {
     public:
-        void setSize(int x, int width, int y, int height) {
-            this->vX = x;
-            this->vWidth = width;
-            this->vY = y;
-            this->vHeight = height;
+        void setLowerBorder(Energyleaf::Stream::V1::Types::Pixel::HSV&& hsv) {
+            this->vLowerBorder = hsv;
+        }
+
+        void setHigherBorder(Energyleaf::Stream::V1::Types::Pixel::HSV&& hsv) {
+            this->vHigherBorder = hsv;
+        }
+
+        const Energyleaf::Stream::V1::Types::Pixel::HSV& getLowerBorder() {
+            return this->vLowerBorder;
+        }
+
+        const Energyleaf::Stream::V1::Types::Pixel::HSV& getHigherBorder() {
+            return this->vHigherBorder;
         }
     private:
-        int vX = 0;
-        int vWidth = 0;
-        int vY = 0;
-        int vHeight = 0;
+        Energyleaf::Stream::V1::Types::Pixel::HSV vLowerBorder = Energyleaf::Stream::V1::Types::Pixel::HSV(0.f,0.f,0.f);
+        Energyleaf::Stream::V1::Types::Pixel::HSV vHigherBorder = Energyleaf::Stream::V1::Types::Pixel::HSV(0.f,0.f,0.f);;
     protected:
-        void work(Energyleaf::Stream::V1::Tuple::Tuple<Energyleaf::Stream::V1::Types::Image> &inputTuple,
-                  Energyleaf::Stream::V1::Tuple::Tuple<Energyleaf::Stream::V1::Types::Image> &outputTuple) override {
-            Energyleaf::Stream::V1::Types::Image img = inputTuple.getItem<Energyleaf::Stream::V1::Types::Image>(0).getData();
+        void work(Energyleaf::Stream::V1::Tuple::Tuple<Energyleaf::Stream::V1::Types::Image,std::string> &inputTuple,
+                  Energyleaf::Stream::V1::Tuple::Tuple<std::size_t,std::string> &outputTuple) override {
+            std::size_t foundPixel = 0;
 
+            Energyleaf::Stream::V1::Types::Image image = inputTuple.getItem<Energyleaf::Stream::V1::Types::Image>(1).getData();
+            Energyleaf::Stream::V1::Types::Pixel::HSV hsv;
+            for (std::size_t i = 0; i < (image.getWidth() * image.getHeight()); ++i) {
+                std::size_t index = i * image.getBytesPerPixel();
+                // Convert RGB to HSV
 
-            if (this->vX < 0 || this->vWidth <= 0 || this->vX + this->vWidth > img.getWidth() ||
-                this->vY < 0 || this->vHeight <= 0 || this->vY + this->vHeight > img.getHeight()) {
-                return;
+                Energyleaf::Stream::V1::Extras::Converter::Types::Pixel::RGBtoHSV::convert(Energyleaf::Stream::V1::Types::Pixel::RGB(image.getData()[index],image.getData()[index+1],image.getData()[index+2]),hsv);
+
+                // Check if the pixel is in the red color range in HSV space
+                if ((hsv.getH() >= this->vLowerBorder.getH() && hsv.getH() <= this->vHigherBorder.getH()) &&
+                    (hsv.getS() >= this->vLowerBorder.getS() && hsv.getS() <= this->vHigherBorder.getS()) &&
+                    (hsv.getV() >= this->vLowerBorder.getV() && hsv.getV() <= this->vHigherBorder.getV())) {
+                    ++foundPixel;
+                }
             }
-
-            Energyleaf::Stream::V1::Types::Image outImg(this->vWidth,this->vHeight,img.getBytesPerPixel(),img.getFormat(),
-                                                        new uint8_t[this->vWidth * this->vHeight * img.getBytesPerPixel()]);
-
-            int startIdxX = this->vX * img.getBytesPerPixel();
-
-            int bytesPerRow = this->vWidth * img.getBytesPerPixel();
-
-            for (int y = 0; y < this->vHeight; ++y) {
-                int inIdx = startIdxX + (y + this->vY) * img.getWidth() * img.getBytesPerPixel();
-                int outIdx = y * bytesPerRow;
-
-                std::copy(img.getData() + inIdx, img.getData() + inIdx + bytesPerRow, outImg.getData() + outIdx);
-            }
-
+            log_d("Found red pixel: %d", foundPixel);
             outputTuple.clear();
-            outputTuple.addItem(std::string("Image"),outImg);
+            outputTuple.addItem(inputTuple.getItem<std::string>(0));
+            outputTuple.addItem(std::string("FOUNDPIXEL"),foundPixel);
         }
     };
 }
 
-#endif //STREAM_V1_CORE_OPERATOR_PIPEOPERATOR_CROPPIPEOPERATOR_HPP
+#endif //STREAM_V1_CORE_OPERATOR_PIPEOPERATOR_DETECTORPIPEOPERATOR_HPP
