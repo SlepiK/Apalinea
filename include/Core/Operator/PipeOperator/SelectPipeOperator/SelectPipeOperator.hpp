0ae278a919b204497fab643fdb569a72cb8cbac6
Simon <simon.stahmer1@uni-oldenburg.de>
2024-03-26 04:28:37 +0100

    to transform

--- include/Core/Operator/PipeOperator/SelectPipeOperator/SelectPipeOperator.hpp
+++ include/Core/Operator/PipeOperator/SelectPipeOperator/SelectPipeOperator.hpp
@@ -8,6 +8,8 @@
 #include "Operator/PipeOperator/AbstractPipeOperator.hpp"
 #include "Tuple/Tuple.hpp"
 
+#include "Types/Datatype/DtBool.hpp"
+
 namespace Energyleaf::Stream::V1::Core::Operator::PipeOperator {
     class SelectPipeOperator
             : public Energyleaf::Stream::V1::Operator::AbstractPipeOperator {
@@ -29,6 +31,31 @@ namespace Energyleaf::Stream::V1::Core::Operator::PipeOperator {
             outputTuple.clear();
             /*outputTuple.addItem(inputTuple.getItem<std::string>(0));
             outputTuple.addItem(std::string("SELECT"), select);*/
+
+            try {
+                if (this->expression) {
+                    this->expression->setTuple(inputTuple);
+                    this->expression->execute();
+                    if (Types::Datatype::DtRegistry::get(this->expression->getIdentifier()) ==
+                        Types::Datatype::DtRegistry::get(Types::Datatype::DtBool::IDENTIFIER)) {
+
+                        bool eval = static_cast<const Types::Datatype::DtBool&>(this->expression->getData()).toBool();
+                        outputTuple.clear();
+                        if(eval) {
+                            outputTuple.addItem(std::string("SELECT"),Types::Datatype::DtBool(eval));
+                            vProcessState = Energyleaf::Stream::V1::Operator::OperatorProcessState::CONTINUE;
+                        } else {
+                            vProcessState = Energyleaf::Stream::V1::Operator::OperatorProcessState::BREAK;
+                        }
+                    } else {
+                        vProcessState = Energyleaf::Stream::V1::Operator::OperatorProcessState::BREAK;
+                    }
+                } else {
+                    throw std::runtime_error("No expression was used!");
+                }
+            } catch (const std::exception& e) {
+                vProcessState = Energyleaf::Stream::V1::Operator::OperatorProcessState::STOP;
+            }
         }
     };
 }
