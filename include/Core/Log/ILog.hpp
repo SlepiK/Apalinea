396662821fd014c638e350c2ac3432698720a905
Simon <simon.stahmer1@uni-oldenburg.de>
2024-06-18 19:10:40 +0200

    feat: adding log path

---
+++ include/Core/Log/ILog.hpp
@@ -0,0 +1,16 @@
+//
+// Created by SlepiK on 18.06.24.
+//
+
+#ifndef STREAM_V1_CORE_ILOG_HPP
+#define STREAM_V1_CORE_ILOG_HPP
+
+namespace Energyleaf::Stream::V1::Core::Log {
+    class ILog {
+    public:
+    private:
+    protected:
+    };
+}
+
+#endif //STREAM_V1_CORE_ILOG_HPP
